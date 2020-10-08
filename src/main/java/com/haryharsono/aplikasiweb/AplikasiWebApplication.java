9595821334bd98702b795056b6d762868e83d346
haryharsono <311098haryharsono@gmail.com>
2020-10-08 11:45:31 +0800

    set up projek

---
+++ src/main/java/com/haryharsono/aplikasiweb/AplikasiWebApplication.java
@@ -0,0 +1,13 @@
+package com.haryharsono.aplikasiweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikasiWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikasiWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haryharsono/aplikasiweb/AplikasiWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haryharsono.aplikasiweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplikasiWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
